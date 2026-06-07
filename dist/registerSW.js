499dde7bb5afc7528ef285a6bbb566a52ce8ae39
Saurabh Panchal <40832570+saurabhpan98@users.noreply.github.com>
2026-06-07 09:09:11 +0000

    error fix and build

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/FileSync/sw.js', { scope: '/FileSync/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
